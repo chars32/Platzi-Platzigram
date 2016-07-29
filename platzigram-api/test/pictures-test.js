8c44b4c48c0c36dd20d0aee56dad05ff6758b99b
chars32 <chars24@gmail.com>
2016-07-29 01:25:43 -0500

    Crecion de endpoint GET/picture (falla el NODE_ENV)

--- platzigram-api/test/pictures-test.js
+++ platzigram-api/test/pictures-test.js
@@ -3,15 +3,16 @@
 // Syntaxis emascript modules
 import test from 'ava'
 import micro from 'micro' // send es un metodo de micro para enviar datos
-import uuid from 'uuid-base62'
 import listen from 'test-listen' // Herramienta para hacer testing de microservicios con micro
 import request from 'request-promise' // Modulo que me permite httpRequest con Promise (promesas)
+import fixtures from './fixtures/'
 import pictures from '../pictures'
 
 // Tes basico para chequear que funcione
 // obtener imagen por id
 test('GET /:id', async t => {
-  let id = uuid.v4()
+  // let id = uuid.v4()
+  let image = fixtures.getImage()
 
   // Creamos un servidor con micro el cual puede ser asincrono
   let srv = micro(pictures)
@@ -19,9 +20,9 @@ test('GET /:id', async t => {
   // listen recibe el srv y crea una url para probarlo
   let url = await listen(srv)
   // Hacemos la peticion http
-  let body = await request({ uri: `${url}/${id}`, json: true })
+  let body = await request({ uri: `${url}/${image.publicId}`, json: true })
   // validamos que el body que retorna es igual al id
-  t.deepEqual(body, { id: id })
+  t.deepEqual(body, image)
 })
 
 // Caracteristica de Ava, se puede definir los test sin necesidad
