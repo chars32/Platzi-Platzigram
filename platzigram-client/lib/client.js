80cecfb28eed7296fd0ed7cc2d195de245d48945
chars32 <chars24@gmail.com>
2016-08-03 23:01:45 -0500

    Implementando el perfil del usuario

--- platzigram-client/lib/client.js
+++ platzigram-client/lib/client.js
@@ -1,9 +1,29 @@
 'use strict'
 
+const request = require('request-promise')
+const Promise = require('bluebird')
+
 // Seteando de manera basica el client
 class Client {
+  constructor (options) {
+    this.options = options || {
+      endpoint: {
+        pictures: 'http://api.platzigram.com/picture',
+        users: 'http://api.platzigram.com/user',
+        auth: 'http://api.platzigram.com/auth'
+      }
+    }
+  }
 
-  getPicture () {}
+  getPicture (id, callback) {
+    let opts = {
+      method: 'GET',
+      uri: `${this.options.endpoints.pictures}/${id}`,
+      json: true
+    }
+
+    return Promise.resolve(request(opts)).asCallback(callback)
+  }
 
   savePicture () {}
 
--- platzigram-client/test/client-test.js
+++ platzigram-client/test/client-test.js
@@ -2,7 +2,22 @@
 
 const test = require('ava')
 const platzigram = require('../')
-// const fixtures = require('./fixtures')
+const fixtures = require('./fixtures')
+// nock lo que hace es hacer mocking de un servidor http
+const nock = require('nock')
+
+let options = {
+  endpoints: {
+    pictures: 'http://platzigram.test/picture',
+    users: 'http://platzigram.test/user',
+    auth: 'http://platzigram.test/auth'
+  }
+}
+
+// Test para que cada que se ejecute un test se instancie un contexto
+test.beforeEach(t => {
+  t.context.client = platzigram.createClient(options)
+})
 
 // Test basico para iniciar el client
 test('client', t => {
@@ -16,4 +31,19 @@ test('client', t => {
   t.is(typeof client.saveUser, 'function')
   t.is(typeof client.getUser, 'function')
   t.is(typeof client.auth, 'function')
+})
+ // Test obtener las imagenes
+test('getPicture', async t => {
+  const client = t.context.client
+
+  let image = fixtures.getImage()
+
+ // Creamos un servidor falso (mock)
+  nock(options.endpoints.pictures)
+    .get(`/${image.publicId}`)
+    .reply(200, image)
+
+  let result = await client.getPicture(image.publicId)
+
+  t.deepEqual(image, result)
 })
